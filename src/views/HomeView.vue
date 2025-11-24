095df1dfacfaa85ee10c27eb2632ff11390d9988
achautemps <chautemps.alexandre@gmail.com>
2025-11-24 16:39:05 +0100

    feat(form) :add form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,21 @@
 import MyButton from '@/components/MyButton.vue'
 import MySimpsons from '@/components/MySimpsons.vue'
 import MyTitle from '@/components/MyTitle.vue'
+import MyForm from '@/components/MyForm.vue'
 </script>
 
 <template>
   <main>
     <MyTitle as="h1">Homepage</MyTitle>
 
-    <MyButton href="/about">Click me 1</MyButton>
+    <MyForm />
+
+    <!-- <MyButton href="/about">Click me 1</MyButton>
     <MyButton href="/about" variant="outline">Click me 1 Outline</MyButton>
     <MyButton href="/about" variant="outline" size="small">Click me Outline small</MyButton>
     <MyButton href="/about" size="small">Click me small</MyButton>
     <MyButton>Click me 2</MyButton>
 
-    <MySimpsons />
+    <MySimpsons /> -->
   </main>
 </template>
