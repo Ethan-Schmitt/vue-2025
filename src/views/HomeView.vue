8747a072bb66aa67edbfaa02322b790e349dbbba
Ethan <ethanschmitt11@gmail.com>
2025-11-24 11:07:26 +0100

    feat: add button styles and responsive mixins
    - Created _button.scss for button styles including small, large, and outline variants.
    - Implemented rem function in _functions.scss for responsive font sizing.
    - Added responsive mixins in _mixins.scss for various breakpoints.
    - Defined breakpoints and primary color variable in _variables.scss.
    - Added a simple logo SVG file.
    - Updated vue-global.d.ts for TypeScript support in Vue components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <button>click me</button>
+  <a href="">click me</a>
 </template>
