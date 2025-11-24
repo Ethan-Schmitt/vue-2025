ed14ed0518475f6abea8722fb627f6e9e2b0f313
achautemps <chautemps.alexandre@gmail.com>
2025-11-24 14:02:37 +0100

    feat(header) : add Nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import MyHeader from './components/MyHeader.vue'
 </script>
 
 <template>
   <header>
+    <MyHeader />
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
