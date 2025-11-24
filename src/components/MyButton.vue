a7b2ac65ee7130e23b082e07807145198514b7d5
achautemps <chautemps.alexandre@gmail.com>
2025-11-24 11:43:00 +0100

    style(button) : add primary variant

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -10,11 +10,21 @@ defineProps<{
 </template>
 
 <style lang="scss">
+@use '@/assets/foundations/functions' as *;
 .button {
-  padding: 10px 20px;
-  background: black;
-  color: white;
-  border: none;
-  border-radius: 4px;
+  --button-color: var(--color-text);
+  --button-bg-color: var(--color-primary);
+  --button-border-color: var(--color-primary);
+  padding: rem(10) rem(20);
+  background: var(--button-bg-color);
+  color: var(--button-color);
+  border: rem(1) solid var(--button-border-color);
+  border-radius: rem(4);
+  text-decoration: none;
+  font-size: var(--font-size-base);
+  &:hover {
+    --button-color: var(--color-primary);
+    --button-bg-color: transparent;
+  }
 }
 </style>
