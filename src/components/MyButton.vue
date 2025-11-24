7364ec2e8ecd4b8f6bee8161a8fd5e10b26966ae
achautemps <chautemps.alexandre@gmail.com>
2025-11-24 11:04:21 +0100

    feat(button) : add button template

---
+++ src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  href?: string
+}>()
+</script>
+
+<template>
+  <a v-if="href" href="" class="button"><slot></slot></a>
+  <button v-else class="button"><slot></slot></button>
+</template>
+
+<style lang="scss">
+.button {
+  padding: 10px 20px;
+  background: black;
+  color: white;
+  border: none;
+  border-radius: 4px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MyButton from '@/components/MyButton.vue'
+</script>
 
 <template>
   <main>
     <h1>Welcome to the Home Page</h1>
+
+    <MyButton href="/about">Click me 1</MyButton>
+    <MyButton>Click me 2</MyButton>
   </main>
 </template>
