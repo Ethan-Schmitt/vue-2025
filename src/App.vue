6b04e47d758e5102befc5cc4c5fe91dc01be9010
Ethan <ethanschmitt11@gmail.com>
2025-11-25 08:28:38 +0100

    maj

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import MyHeader from './components/MyHeader.vue'
-import DefaultLayout from './layouts/DefaultLayout.vue'
 </script>
 
 <template>
   <DefaultLayout>
-    <template #header>
-      <MyHeader />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </template>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
     <RouterView />
-    <template #aside>
-      <p>Aside</p>
-    </template>
-    <template #footer>
-      <p>Footer</p>
-    </template>
-    <p>paragraphe</p>
   </DefaultLayout>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,5 @@
-<script setup lang="ts">
-import MyButton from '@/components/MyButton.vue'
-import MySimpsons from '@/components/MySimpsons.vue'
-import MyTitle from '@/components/MyTitle.vue'
-import MyForm from '@/components/MyForm.vue'
-</script>
-
 <template>
   <main>
-    <MyTitle as="h1">Homepage</MyTitle>
-
-    <MyForm />
-
-    <!-- <MyButton href="/about">Click me 1</MyButton>
-    <MyButton href="/about" variant="outline">Click me 1 Outline</MyButton>
-    <MyButton href="/about" variant="outline" size="small">Click me Outline small</MyButton>
-    <MyButton href="/about" size="small">Click me small</MyButton>
-    <MyButton>Click me 2</MyButton>
-
-    <MySimpsons /> -->
+    <h1>HP</h1>
   </main>
 </template>
